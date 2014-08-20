1bca16522d62b938e22b70dbfe20463cd07d3436
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2014-08-20 17:34:19 -0400

    Now sending more metrics and all types in the test message

--- send_test_msg.sh
+++ send_test_msg.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-echo "Sending a counter increment message..."
+echo "Sending metrics..."
+
 echo "testing.counter:$(($RANDOM % 10))|c" | nc -w 1 -u 127.0.0.1 8125
+
+for var in 1 2 3 4 5 6 7 8 9 10; do
+  echo "testing.timer:$(($RANDOM % 50))|ms" | nc -w 1 -u 127.0.0.1 8125
+done
+
+echo "testing.gauge:$(($RANDOM % 100))|g" | nc -w 1 -u 127.0.0.1 8125
