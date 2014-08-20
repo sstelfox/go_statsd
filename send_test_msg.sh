32afc0f020a69c0825360d8722d5dd0c60d1aea1
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2014-08-20 17:44:00 -0400

    Shortened up the test message loop

--- send_test_msg.sh
+++ send_test_msg.sh
@@ -4,7 +4,7 @@ echo "Sending metrics..."
 
 echo "testing.counter:$(($RANDOM % 10))|c" | nc -w 1 -u 127.0.0.1 8125
 
-for var in 1 2 3 4 5 6 7 8 9 10; do
+for var in {1..10}; do
   echo "testing.timer:$(($RANDOM % 50))|ms" | nc -w 1 -u 127.0.0.1 8125
 done
 
